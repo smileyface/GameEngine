ff0335f491fec01e066bd3d2882f6cef902ca9c1
Kason Bennett <kason.bennett@aggiemail.usu.edu>
2017-05-24 22:23:27 -0700

    Added the concept of a timed offset

--- Football-RPG/Command.cpp
+++ Football-RPG/Command.cpp
@@ -6,6 +6,11 @@ Command::Command()
 {
 }
 
+Command::Command(int offset)
+{
+	Command::command_offset = offset;
+}
+
 
 Command::~Command()
 {
@@ -14,4 +19,12 @@ Command::~Command()
 std::shared_ptr<Command> CommandInterface::create_command()
 {
 	return std::shared_ptr<Command>();
+}
+
+/*
+offset is in miliseconds
+*/
+std::shared_ptr<Command> CommandInterface::create_command(int offset)
+{
+	return std::shared_ptr<Command>(offset);
 }
--- Football-RPG/Command.h
+++ Football-RPG/Command.h
@@ -9,12 +9,15 @@ class Command
 {
 public:
 	Command();
+	Command(int offset);
 	~Command();
 	long commanded_id;
+	int command_offset;
 };
 
 namespace CommandInterface
 {
 	std::shared_ptr<Command> create_command();
+	std::shared_ptr<Command> create_command(int offset);
 }
 
