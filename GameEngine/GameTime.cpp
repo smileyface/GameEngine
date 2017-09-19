c3058e3304fda7c59444782e8c6fb528bb9fd4cd
Kason Bennett <kason.bennett@aggiemail.usu.edu>
2017-09-18 22:53:48 -0700

    Added Stop and Running options

--- GameEngine/GameTime.cpp
+++ GameEngine/GameTime.cpp
@@ -1,19 +1,37 @@
 #include "GameTime.h"
 
 /*
+Default Constructor
+
+sets previous time to time of construction. 
+*/
+RunningTime::RunningTime()
+{
+	previous_time = std::chrono::steady_clock::now();
+	step_length = -1.0f;
+}
+
+/*
 Update the model time.
 
 Elapsed time from the last step to this step.
 */
-void Time::update_time()
+void RunningTime::update_time()
 {
-	auto now_time = std::chrono::steady_clock::now();
-	auto elapsed_time = std::chrono::duration_cast<std::chrono::duration<float>>(now_time - Time::previous_time);
-	Time::time _+= elapsed_time.count();
-	Time::previous_time = now_time;
+	auto current_time = std::chrono::steady_clock::now();
+	auto elapsed_time = std::chrono::duration_cast<std::chrono::duration<float>>(current_time - RunningTime::previous_time);
+	RunningTime::elapsed_time = elapsed_time.count;
+	RunningTime::overall_time_used += elapsed_time.count();
+	RunningTime::previous_time = current_time;
 }
 
-void Time::update_time(int hertz)
+/*
+Set the step length to the hertz.
+*/
+void RunningTime::change_step_hz(int hertz)
 {
-	double hz = 1.0 / hertz;
+	RunningTime::step_length = 1.0f / hertz;
 }
+
+
+
--- GameEngine/GameTime.h
+++ GameEngine/GameTime.h
@@ -1,14 +1,39 @@
 #pragma once
 
 #include <chrono>		// steady_clock | duration_cast | duration
+#include <vector>		// vector
+#include <map>			// map
+#include <string>		// string
 
 class Time
 {
+public:
+	virtual void update_time() = 0;
+};
+
+class RunningTime : public Time
+{
 private:
-	float time;
 	std::chrono::steady_clock::time_point previous_time;
+	float step_length = -1.0f;
 
 public:
+	RunningTime();
 	void update_time();
-	void update_time(int hertz);
+	void change_step_hz(int hertz);
+
+	float overall_time_used;
+	float elapsed_time;
 };
+
+class StoppedTime : public Time
+{
+public:
+	void update_time() {};
+};
+
+namespace Clocks
+{
+	RunningTime main_clock;
+	std::vector<Time> time_pieces;
+}
