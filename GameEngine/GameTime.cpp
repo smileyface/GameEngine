b16223bc672e9e71c20071847917037868305dd0
Kason Bennett <kason.bennett@aggiemail.usu.edu>
2017-09-19 00:29:34 -0700

    Basic functionality implemented

--- GameEngine/GameTime.cpp
+++ GameEngine/GameTime.cpp
@@ -5,7 +5,7 @@ Default Constructor
 
 sets previous time to time of construction. 
 */
-RunningTime::RunningTime()
+Time::Time()
 {
 	previous_time = std::chrono::steady_clock::now();
 	step_length = -1.0f;
@@ -16,22 +16,36 @@ Update the model time.
 
 Elapsed time from the last step to this step.
 */
-void RunningTime::update_time()
+void Time::update_time()
 {
 	auto current_time = std::chrono::steady_clock::now();
-	auto elapsed_time = std::chrono::duration_cast<std::chrono::duration<float>>(current_time - RunningTime::previous_time);
-	RunningTime::elapsed_time = elapsed_time.count;
-	RunningTime::overall_time_used += elapsed_time.count();
-	RunningTime::previous_time = current_time;
+	auto elapsed_time = std::chrono::duration_cast<std::chrono::duration<float>>(current_time - Time::previous_time);
+	Time::elapsed_time = elapsed_time.count;
+	Time::overall_time_used += elapsed_time.count();
+	Time::previous_time = current_time;
 }
 
 /*
 Set the step length to the hertz.
 */
-void RunningTime::change_step_hz(int hertz)
+void Time::change_step_hz(int hertz)
 {
-	RunningTime::step_length = 1.0f / hertz;
+	Time::step_length = 1.0f / hertz;
 }
 
+void Time::time_stop()
+{
+	Time::update = []() {};
+}
 
-
+void Time::time_start()
+{
+	update = [this] ()
+	{
+		auto current_time = std::chrono::steady_clock::now();
+		auto elapsed_time = std::chrono::duration_cast<std::chrono::duration<float>>(current_time - Time::previous_time);
+		Time::elapsed_time = elapsed_time.count;
+		Time::overall_time_used += elapsed_time.count();
+		Time::previous_time = current_time;
+	};
+}
--- GameEngine/GameTime.h
+++ GameEngine/GameTime.h
@@ -1,39 +1,27 @@
 #pragma once
 
+#include <mutex>
+#include <functional>	// function
 #include <chrono>		// steady_clock | duration_cast | duration
 #include <vector>		// vector
 #include <map>			// map
 #include <string>		// string
 
+
+
 class Time
 {
-public:
-	virtual void update_time() = 0;
-};
-
-class RunningTime : public Time
-{
-private:
 	std::chrono::steady_clock::time_point previous_time;
 	float step_length = -1.0f;
-
-public:
-	RunningTime();
 	void update_time();
+
+	Time();
+	std::function<void()> update = [] {};
 	void change_step_hz(int hertz);
+	void time_stop();
+	void time_start();
+
 
 	float overall_time_used;
 	float elapsed_time;
 };
-
-class StoppedTime : public Time
-{
-public:
-	void update_time() {};
-};
-
-namespace Clocks
-{
-	RunningTime main_clock;
-	std::vector<Time> time_pieces;
-}
