dfb22270ff66d0ffab99f604cb912dd0c83af515
Kason Bennett <kason.bennett@aggiemail.usu.edu>
2018-03-26 23:45:28 -0700

    Testing Thread Manager

---
+++ SystemTests/ThreadManagerTest.cpp
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "../GameEngine/include/ThreadManager.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SystemTests
+{		
+	TEST_CLASS(ThreadManager)
+	{
+	public:
+		
+		TEST_METHOD(add_job)
+		{
+			//TODO: Without Level
+			//TODO: With Level
+			//TODO: With two different levels
+			//TODO: With two same levels
+			//TODO: Lambda
+			//TODO: Handle interupts
+			Assert::Fail;
+		}
+
+	};
+
+	TEST_CLASS(Job)
+	{
+	public:
+		TEST_METHOD(create)
+		{
+			Assert::Fail;
+		}
+	};
+}
